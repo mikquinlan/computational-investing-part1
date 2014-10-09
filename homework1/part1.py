Get the correct optimised portfolio answer but the allocations still have decimal overflow

homework1/part1.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import time
-import itertools as it
-from decimal import Decimal
+import itertools
+from decimal import *
 
 class Simulator:
 
@@ -143,26 +143,7 @@ class Simulator:
 
         increments = np.arange(0, 1.1, 0.1)
         # combinations = it.combinations(range, len(ls_symbols))
-        permutations = it.combinations_with_replacement(increments, len(ls_symbols))
-        rounded_permutations = []
-        for permutation in permutations:
-            rounded = []
-            for entry in permutation:
-                rounded.append(float(Decimal(str(entry)).quantize(Decimal('0.1'))))
-            rounded_permutations.append(rounded)
-        print "Rounded permutations"
-        print (rounded_permutations)
-        #Filter out those combinations that don't add up to 1.0
-        whole_portfolio_combinations = []
-        for combination in rounded_permutations:
-            sum_allocations = 0
-            for x in combination:
-                sum_allocations += x
-            if sum_allocations == 1.0:
-                whole_portfolio_combinations.append(combination)
-            else:
-                print "Discarding"
-                print (combination)
+        valid_portfolio_allocations = self.discover_valid_allocation_combinations()
 
         #Now have all the combinations that add up to 1, get the data then calculate figures and save the best sharp ratio
         d_data = self.readData(start_date, end_date, ls_symbols)[0]
@@ -170,10 +151,7 @@ class Simulator:
         na_normalized_price = na_price / na_price[0, :]
         best_allocation = []
         best_sharp_ratio = 0
-        print "Combinations to be analyzed"
-        for combination in whole_portfolio_combinations:
-            print (combination)
-        for combination in whole_portfolio_combinations:
+        for combination in valid_portfolio_allocations:
             f_portf_sharpe = self.calc_stats(na_normalized_price, combination)[2]
             if f_portf_sharpe > best_sharp_ratio:
                 best_sharp_ratio = f_portf_sharpe
@@ -182,6 +160,44 @@ class Simulator:
         print "\nBest Sharpe Ratio: %s" % best_sharp_ratio
         print "\nBest allocation:"
         print (best_allocation)
+
+    def discover_valid_allocation_combinations(self):
+        print "discover"
+        min = 0.0
+        max = 1.0
+        increment = 0.1
+
+        valid_allocations = []
+        candidate_allocation = [0.0, 0.0, 0.0, 0.0]
+
+        for first_allocation in self.drange(min, max, increment):
+            for second_allocation in self.drange(min, max, increment):
+                for third_allocation in self.drange(min, max, increment):
+                    for fourth_allocation in self.drange(min, max, increment):
+                        candidate_allocation[0] = first_allocation
+                        candidate_allocation[1] = second_allocation
+                        candidate_allocation[2] = third_allocation
+                        candidate_allocation[3] = fourth_allocation
+                        if self.is_valid_allocation(candidate_allocation):
+                            valid_allocations.append(list(candidate_allocation))
+
+        return valid_allocations
+
+    def drange(self, start, stop, step):
+        r = start
+        while r < stop:
+            yield r
+            r += step
+
+    def is_valid_allocation(self, allocations):
+        if len(allocations) == 4:
+            sum_allocations = 0.0
+            for x in allocations:
+                sum_allocations += x
+            if sum_allocations == 1.0:
+                return True
+
+        return False
 
 
 if __name__ == '__main__':
@@ -194,3 +210,4 @@ if __name__ == '__main__':
 
     # volatility_std_dev, daily_ret, sharpe, cum_ret = simulator.simulate(start_date, end_date, symbols, allocations, True)
     simulator.optimize(start_date, end_date, symbols)
+