3910d587675196f34b30c83cc9175bdb7ed86912
Mik Quinlan <mik.quinlan@gmail.com>
2014-10-19 19:28:42 -0700

    Saved point. Need to calculate daily share allocation, cash balances and equity balances. Implement owned_shares_per_day next.

--- homework3/marketsim.py
+++ homework3/marketsim.py
@@ -2,16 +2,67 @@ __author__ = 'mquinlan'
 
 import sys
 import csv
-import datetime
+import QSTK.qstkutil.qsdateutil as du
+import datetime as dt
+import QSTK.qstkutil.DataAccess as da
 import collections
+from decimal import *
+import json
+
 
 class MarketSimulator:
     def run(self, initial_cash_amount, orders_file, output_file):
         symbols = self.extract_symbols(orders_file)
         print 'Symbols %s ' % symbols
 
-        orders_per_day = self.collate_orders_per_day(orders_file)
-        print 'Orders per day %s ' % orders_per_day
+        orders_by_date = self.collate_orders_per_day(orders_file)
+        print 'Orders per day %s ' % orders_by_date
+
+        all_trade_dates = orders_by_date.keys()
+        date_start = all_trade_dates[0]
+        date_end = all_trade_dates[len(all_trade_dates) - 1]
+
+        print 'Trade start date: %s ' % date_start
+        print 'Trade end date: %s ' % date_end
+
+        # close is adjusted close according to First Figure subheading of http://wiki.quantsoftware.org/index.php?title=QSTK_Tutorial_1
+        ls_keys = ['close']
+        d_data, trading_days = self.download_data_from_yahoo(date_start, date_end, symbols, ls_keys)
+        print 'Downloaded Yahoo data: %s ' % d_data
+
+        owned_shares = self.owned_shares_per_day(trading_days, orders_by_date)
+        # cash_balances_per_day = self.calculate_cash_balances_per_day(trading_days, initial_cash_amount, owned_shares, d_data['close'])
+        # equity_balances_per_day = self.calculate_equity_balances_per_day(trading_days, owned_shares, d_data['close'])
+        #
+        # daily_portfolio_balance = self.calculate_portfolio_balance(cash_balances_per_day, equity_balances_per_day)
+        #
+        # self.output_balances(daily_portfolio_balance, output_file)
+
+    def owned_shares_per_day(self, trading_days, orders_by_date):
+        #Dictionary of dictionaries keyed by date. Second dimension dictionary is running total of stock and shares for that day
+        share_allocation_by_date = {}
+        for i in range(len(trading_days)):
+            trading_day = trading_days[i]
+
+
+    def download_data_from_yahoo(self, date_start, date_end, symbols, ls_keys):
+        # Yahoo data date_end is exclusive, need to add a day to get the data we want
+        date_end_extension = date_end + dt.timedelta(days=1)
+        ldt_timestamps = du.getNYSEdays(date_start, date_end_extension, dt.timedelta(hours=16))
+
+        symbols.add('SPY')
+        dataobj = da.DataAccess('Yahoo')
+
+        print "Downloading data from Yahoo"
+        ldf_data = dataobj.get_data(ldt_timestamps, symbols, ls_keys)
+        d_data = dict(zip(ls_keys, ldf_data))
+
+        for s_key in ls_keys:
+            d_data[s_key] = d_data[s_key].fillna(method='ffill')
+            d_data[s_key] = d_data[s_key].fillna(method='bfill')
+            d_data[s_key] = d_data[s_key].fillna(1.0)
+
+        return d_data, ldt_timestamps
 
     def extract_symbols(self, orders_file):
         symbols = set()
@@ -29,13 +80,12 @@ class MarketSimulator:
         with open(orders_file, 'rb') as csvfile:
             orders_reader = csv.reader(csvfile, delimiter=',')
             for order in orders_reader:
-                print "order %s " % order
                 order_entry = []
                 order_entry.append(order[3]) # ticker
                 order_entry.append(order[4]) # Buy/Sell
                 order_entry.append(order[5]) # Number shares
 
-                date_of_order = datetime.datetime(int(order[0]), int(order[1]), int(order[2]), 0, 0, 0, 0)
+                date_of_order = dt.datetime(int(order[0]), int(order[1]), int(order[2]), 0, 0, 0, 0)
                 current_orders = orders_by_date.get(date_of_order)
                 if current_orders is None:
                     list_of_orders = []
@@ -45,6 +95,54 @@ class MarketSimulator:
                     current_orders.append(order_entry)
 
         return collections.OrderedDict(sorted(orders_by_date.items()))
+
+    # def calculate_portfolio_balance(self, trading_days, initial_cash_amount, orders_by_date, closing_prices):
+    #
+    #     # Go through each day, check if there's an order, if there is, execute it.
+    #     # End of day balance = cash + n(ticker closing price * shares)
+    #     running_total = Decimal(initial_cash_amount)
+    #     share_allocation = {}
+    #     getcontext().prec = 2
+    #     daily_balances = collections.OrderedDict()
+    #
+    #     #We have to adjust to get the same timestamp as that returned from Yahoo
+    #     now = date_start + date_start.replace(hour=16)
+    #     while(True):
+    #         if(now > date_end):
+    #             break
+    #         now = date_start + dt.timedelta(days=1)
+    #         orders = orders_by_date[now]
+    #         if orders == nil
+    #
+    #
+    #     for order_date in orders_by_date.keys():
+    #         orders = orders_by_date.get(order_date)
+    #         for order in orders:
+    #
+    #             adjusted_order_date = order_date.replace(hour=16)
+    #             ticker = order[0]
+    #             buy_sell = order[1]
+    #             number_shares = order[2]
+    #             closing_prices_for_ticker = closing_prices[ticker]
+    #             closing_price_on_day = closing_prices_for_ticker[adjusted_order_date]
+    #             if buy_sell == 'Buy':
+    #                 running_total += (Decimal(closing_price_on_day) * Decimal(number_shares))
+    #             elif buy_sell == 'Sell':
+    #                 running_total -= (Decimal(closing_price_on_day) * Decimal(number_shares))
+    #             else:
+    #                 raise Exception("Unrecognised buy/sell attribute: %s " % buy_sell)
+    #
+    #             daily_balances[adjusted_order_date] = running_total
+    #
+    #     return daily_balances
+
+    def output_balances(self, daily_portfolio_balances, output_file):
+        with open(output_file, 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            for date_of_balance in daily_portfolio_balances.keys():
+                balance = daily_portfolio_balances.get(date_of_balance)
+                balance_string = "%.2f" % float(balance)
+                writer.writerow([str(date_of_balance), balance_string])
 
 
 if __name__ == '__main__':
